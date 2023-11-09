77308ebf524b3ec8e86102c340f1d0c1749f3cdb
Clemens Quoß <clemens@quoss.de>
2023-11-09 21:35:35 +0100

    WIP - date 11/09/2023, time 21.35

--- CHAPTER_1__A_Tutorial_Introduction/1_2_Variables_and_Arithmetic_Expressions/fahr.c
+++ CHAPTER_1__A_Tutorial_Introduction/1_2_Variables_and_Arithmetic_Expressions/fahr.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 /* print Fahrenheit-Celsius table
- *    for fahr = 0, 20, ..., 300
- */    
-int main() {
-
+   for fahr = 0, 20, ..., 300 */
+main()
+{
 	int fahr, celsius;
 	int lower, upper, step;
-
-	lower = 0; 	/* lower limit of temperature table */
-	upper = 300; 	/* upper limit */
-	step = 20; 	/* step size */
+	lower = 0; /* lower limit of temperature table */
+	upper = 300; /* upper limit */
+	step = 20; /* step size */
 
 	fahr = lower;
 	while (fahr <= upper) {
@@ -19,4 +17,3 @@ int main() {
 		fahr = fahr + step;
 	}
 }
-
